a608cf9ba6950295acd5c2b327d53621a05f92eb
Bordinassi <123772388+jusenai2023@users.noreply.github.com>
2023-08-02 10:52:26 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,87 @@
+//Questão 1 //
+var favoritefood = "pizza";
+
+//Questão 2 //
+var numOfSlices = "8";
+
+// Questão 3 //
+console.log(favoritefood);
+
+// Questão 4 //
+console.log(numOfSlices);
+
+// Questão 5//
+
+/*a principal diferença entre "var" e "let" é que "var" tem escopo de função ou escopo global,
+e sofre de hoisting, enquanto "let" tem escopo de bloco e não sofre de hoisting. 
+Em geral, recomenda-se o uso de "let" em vez de "var" na maioria das situações modernas de desenvolvimento 
+JavaScript, pois "let" é mais seguro e ajuda a evitar problemas comuns associados ao escopo e hoisting.*/
+
+// Questão 6//
+let changeMe =  true;
+
+// Questão 7//
+changeMe = false;
+
+// Questão 8//
+console.log(changeMe);
+
+// Questão 9//
+const entree = 'Enchiladas';
+
+//Questão 10//
+console.log(entree);
+
+// Questão 11//
+const entree = 'Enchiladas';
+entree = 'Tacos'; // Tentando reatribuir um valor diferente
+
+// Após tentar reatribuir um valor diferente 
+
+// Questão 12 //
+
+let levelUp = 10;
+levelUp += 5; 
+console.log(levelUp)
+
+// Questão 13 //
+
+let powerLevel = 10;
+powerLevel -= 100; 
+console.log(powerLevel)
+
+// Questão 14 //
+
+let multiplyMe = 10;
+multiplyMe *= 11; 
+console.log(multiplyMe)
+
+// Questão 15 //
+
+let quarterMe = 10;
+quarterMe /= 4; 
+console.log(quarterMe)
+
+// Questão 16 //
+
+let gainedDollar = 1;
+gainedDollar++;
+console.log(gainedDollar); 
+
+// Questão 17 //
+
+let lostDollar = 5;
+lostDollar--;
+console.log(lostDollar); 
+
+// Questão 18 //
+
+let myName = 'Giovanna';
+let myCity = 'New York';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+// Questão 19 //
+
+let newVariable = 42;
+console.log(typeof newVariable);
+
